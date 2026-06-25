97af470959630f9e5b487463133319e60cd2a12d
Palacio <lucaspalacioa@gmail.com>
2026-06-25 15:21:34 -0300

    First commit, saving context and identifying exception or interruption

--- src/trap.c
+++ src/trap.c
@@ -1,5 +1,7 @@
+#include <kernel/types.h>
 #include <kernel/trap.h>
 #include <kernel/panic.h>
+#include <csr.h>
 
 /* defined in src/trap_entry.S */
 extern void trap_entry();
@@ -18,14 +20,28 @@ void handle_exception()
 
 void trap_setup()
 {
-	/* not implemented */
-	BUG();
+    /* * Write the virtual address of our assembly trap handler into stvec.
+     * By default, writing the raw address sets it to "Direct Mode" 
+     * (all traps jump to this exact address).
+     */
+    csr_write(CSR_STVEC, (u64)trap_entry);
 }
 
 void handle_trap()
 {
-	/* not implemented */
-	BUG();
+    u64 scause = csr_read(CSR_SCAUSE);
+    u64 stval  = csr_read(CSR_STVAL);
+    
+    /* bit 63 indica se foi uma exceção ou interrupção */
+    int is_interrupt = (scause & (1ULL << 63)) != 0;
+    
+    u64 exception_code = scause & ~(1ULL << 63);
+
+    if (is_interrupt) {
+        handle_irq(exception_code);
+    } else {
+        handle_exception(exception_code);
+    }
 }
 
 void hart_irq_enable()
